cf546522bc2cf327b7b29ae2c788c231b95c36b6
Guilherme <guileite97@hotmail.com>
2025-05-20 19:04:53 -0300

    Lista de dados

--- cypress/e2e/3-loja-ebac/produtos.cy.js
+++ cypress/e2e/3-loja-ebac/produtos.cy.js
@@ -27,17 +27,33 @@ beforeEach(() => {
             .click()
     });
 
-    it.only('Deve buscar um produto com sucesso', () => {
+    it('Deve buscar um produto com sucesso', () => {
         let produto = 'Abominable Hoodie'
         produtosPage.buscarProduto(produto)
         cy.get('.product_title').should('contain', produto)
     })
 
     it('Deve visitar a página do produto', () => {
-        
+        produtosPage.visitarProduto('Aero Daily Fitness Tee')
+        cy.get('.product_title').should('contain', 'Aero Daily Fitness Tee')
     })
 
     it('Deve adicionar o produto ao carrinho', () => {
+        let qtd = 2
+        produtosPage.buscarProduto('Cassius Sparring Tank')
+        produtosPage.addProdutoCarrinho('M', 'Blue', qtd)
+        cy.get('.woocommerce-message').should('contain', qtd + ' × “Cassius Sparring Tank” foram adicionados no seu carrinho.')
+    })
+
+    it.only('Deve adicionar o produto ao carrinho buscando da massa de dados', () => {
+        cy.fixture('produtos').then(dados => {
+        produtosPage.buscarProduto(dados[2].nomeProduto)
+        produtosPage.addProdutoCarrinho(
+            dados[2].tamanho, 
+            dados[2].cor, 
+            dados[2].quantidade)
+        cy.get('.woocommerce-message').should('contain', dados[2].nomeProduto)
+        })
         
     })
 });
